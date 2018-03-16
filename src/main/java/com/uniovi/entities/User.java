d51ca1612aec58f1a01a44dfd8ef0234d2c0ad5f
Hugo <alumno214a@gmail.com>
2018-03-16 18:55:06 +0100

    [INI] Creado esqueleto base para la aplicación.
    Ademas se ha completado la primera historia en la que se debia crear el login.

---
+++ src/main/java/com/uniovi/entities/User.java
@@ -0,0 +1,79 @@
+package com.uniovi.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue
+	private long id;
+	@Column(unique = true)
+	private String name;
+	private String email;
+	private String role;
+	private String password;
+	
+	@Transient //propiedad que no se almacena e la tabla.
+	private String passwordConfirm;
+
+	public User(String name, String email) {
+		super();
+		this.name = name;
+		this.email = email;
+	}
+
+	public User() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPasswordConfirm() {
+		return passwordConfirm;
+	}
+	
+	public void setPasswordConfirm(String passwordConfirm) {
+		this.passwordConfirm = passwordConfirm;
+	}
+}
