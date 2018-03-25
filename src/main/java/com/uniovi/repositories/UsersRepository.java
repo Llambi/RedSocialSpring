084bd95c34dbb5d80505c821d4b3d7c36fce1408
MrKarrter <kartertfno@gmail.com>
2018-03-25 12:07:26 +0200

    Actualizados los repositorios para soportar invitaciones

--- src/main/java/com/uniovi/repositories/UsersRepository.java
+++ src/main/java/com/uniovi/repositories/UsersRepository.java
@@ -1,4 +1,5 @@
 package com.uniovi.repositories;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -6,14 +7,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.uniovi.entities.User;
 
-public interface UsersRepository extends CrudRepository<User, Long>{	
-	
+public interface UsersRepository extends CrudRepository<User, Long> {
+
 	User findByEmail(String email);
-	
-	Page<User> findAll(Pageable pageable); 
+
+	Page<User> findAll(Pageable pageable);
 
 	@Query("SELECT r FROM User r WHERE (LOWER(r.name) LIKE LOWER(?1) OR LOWER(r.email) LIKE LOWER(?1))")
 	Page<User> searchUsersByNameAndEmail(Pageable pageable, String seachtext);
-	
-	
 }
